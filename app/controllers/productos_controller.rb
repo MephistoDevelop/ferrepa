3a75b436e2d89775c9e57a39d1800068c18fac9c
MephistoDevelop <inesherald@gmail.com>
2020-07-28 19:53:54 -0500

    Add action buttons to productos view

--- app/controllers/productos_controller.rb
+++ app/controllers/productos_controller.rb
@@ -12,9 +12,16 @@ class ProductosController < ApplicationController
     redirect_to productos_path
   end
 
+  def show
+    @producto_encontrado = Producto.where(['id=?', params[:id]]).first
+    puts @producto_encontrado.nombre
+    # redirect_to productos_path
+    # @producto_encontrado.nombre
+  end
+
   private
 
   def producto_params
-    params.require(:productos).permit(:codigo_barras, :nombre, :unidad, :precio_compra, :precio_medio_mayoreo, :precio_general, :porcentaje_ganancia)
+    params.require(:productos).permit(:id, :codigo_barras, :nombre, :unidad, :precio_compra, :precio_medio_mayoreo, :precio_general, :porcentaje_ganancia)
   end
 end
